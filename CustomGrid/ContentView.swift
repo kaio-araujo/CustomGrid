f6157c52439c4e37bb62f780b3d1043ac67d92e7
Kaio <kaioaguanais@gmail.com>
2020-09-24 11:29:04 -0700

    Initial Commit

---
+++ CustomGrid/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CustomGrid
+//
+//  Created by Kaio Guanais on 2020-09-24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ CustomGrid/CustomGridApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomGridApp.swift
+//  CustomGrid
+//
+//  Created by Kaio Guanais on 2020-09-24.
+//
+
+import SwiftUI
+
+@main
+struct CustomGridApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
